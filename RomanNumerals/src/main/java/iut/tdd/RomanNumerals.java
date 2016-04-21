95f0d9dbdb0ff8a1adbbd72c1845925f747ca229
Melissa Maerten <me.maerten@gmail.com>
2016-04-21 14:19:49 +0200

    Ajout dossiers

---
+++ RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
@@ -0,0 +1,12 @@
+package iut.tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		return "I";
+	}
+
+	public Object convertFromRoman(String roman) {
+		return "1";
+	}
+
+}
